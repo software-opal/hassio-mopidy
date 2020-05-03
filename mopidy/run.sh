9e622115331a41b02f89b76aac23afda7412aefd
Opal Symes <code@opal.codes>
2020-05-03 17:02:03 +1200

    Fix verbose flag

--- mopidy/run.sh
+++ mopidy/run.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 cat /data/options.json | jq .
 
 options=$(cat /data/options.json | jq -r 'if .options then [.options[] | "-o "+.name+"="+.value ] | join(" ") else "" end')
-verbose=$(cat /data/options.json | jq -r '.verbose | (if .|not then "" elif . >= 1 then "-" + ([range(.)] | map("v") | join("")) else "-v" end)' )
+verbose=$(cat /data/options.json | jq -r '.verbose | (if . >= 1 then "-" + ([range(.)] | map("v") | join("")) else "" end)' )
 config="/var/lib/mopidy/.config/mopidy/mopidy.conf"
 
 mopidy $verbose --config $config $options
