1683b7b51ad94387a15a790320f95274caef4eb2
Opal Symes <code@opal.codes>
2020-05-03 16:13:20 +1200

    Updates

--- mopidy/run.sh
+++ mopidy/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
+cat /data/options.json | jq
+
 options=$(cat /data/options.json | jq -r 'if .options then [.options[] | "-o "+.name+"="+.value ] | join(" ") else "" end')
 verbose=$(cat /data/options.json | jq -r '.verbose | (if .|not then "" elif . >= 1 then "-" + ([range(.)] | map("v") | join("")) else "-v" end)' )
 config="/var/lib/mopidy/.config/mopidy/mopidy.conf"
